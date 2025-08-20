c52cb52ae4fc0b4b5dc6acb17627c746e45475d0
AfaqAliChishti <mianafaqalichishti@gmail.com>
2025-08-20 10:00:07 +0500

    fixing some style issuse

--- Project 4 (Calculator)/script.js
+++ Project 4 (Calculator)/script.js
@@ -1,9 +1,13 @@
+// assinging variable to accessing objects
 const input = document.getElementById('input');
 const button = document.querySelectorAll('button');
 
+// appling forEach loop because button is a node list or array 
 button.forEach(btn => {
+    // appling addListener to button
     btn.addEventListener('click',() => {
         const value = btn.innerHTML;
+        // appling condition for calculator
         if(value==="AC"){
             input.value = "";
         }
@@ -16,6 +20,7 @@ button.forEach(btn => {
         else{
          input.value += value;   
         }
+        // the below line is not important but its show your inputs on browser console
         console.log(`inner text is ${input.value}`);
     })
 });
