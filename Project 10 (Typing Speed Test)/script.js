9f456fc386fecc29a50e38768a5d093138f90e89
AfaqAliChishti <mianafaqalichishti@gmail.com>
2025-08-25 21:08:19 +0500

    Project 10 (Typing Speed Test) Some Script Changing

--- Project 10 (Typing Speed Test)/script.js
+++ Project 10 (Typing Speed Test)/script.js
@@ -1,78 +1,74 @@
-const text = document.getElementById("text");
-const input = document.getElementById("input");
-const btn = document.getElementById("button");
+// script.js and scrip2.js are same file script2.js has shorter code and easy to understand
+const givenText = document.getElementById("text");
+const typingArea = document.getElementById("input");
+const startBtn = document.getElementById("button");
 const time = document.getElementById("Time");
 const speed = document.getElementById("Speed");
-const acuracy = document.getElementById("Accuracy");
-// converting sting into array
-let ntext = text.innerText.trim(); //This will remove space from start and end of words
-let words = ntext.split(/\s+/); //This will conver string into array
-text.innerHTML = words.map((w) => `<span>${w}</span>`).join(" "); // this will place every word into span we use these span to color text
-let wordSpans = text.querySelectorAll("span"); // real span elements
-// function for What should happend if we click on start button
-function clicked() {
-  btn.innerHTML = "STARTED";
-  input.value = " ";
-  input.addEventListener("keydown", startTypin);
-  Time();
-}
-let CorrectWord = 0;
-let WrongWord = 0;
-//Checking acuracy
-function chacuracy() {
-  wordSpans.forEach((w) => {
-    if (w.style.color === "green") {
-      CorrectWord++;
-    } else if (w.style.color === "red") {
-      WrongWord++;
-    }
-  });
-}
-//function for what should happend when we start typing
-let TypedWords = 0;
-function startTypin() {
-  let typedtext = input.value.trim();
-  let typedwords = typedtext.split(/\s+/);
-  let wordslenght = typedwords.length;
-  TypedWords = wordslenght;
-  speed.innerHTML = `SPEED <br> ${wordslenght} wpm`;
-  wordSpans.forEach((span, i) => {
-    if (typedwords[i] === words[i]) {
-      span.style.color = "green";
-    } else if (typedwords[i]) {
-      let y = (span.style.color = "red");
+const acuracy = document.getElementById("Acuracy");
+
+// Making array from given text
+let textArray = givenText.innerHTML.trim().split(/\s+/);
+givenText.innerHTML = textArray.map((text) => `<span>${text}</span>`).join(" "); //maping array all element because we cannot apply color on array
+
+//Selecting all words
+const spanArray = givenText.querySelectorAll("span");
+
+// Function what happened when we start typing
+function typing() {
+  let TypedWords = typingArea.value.trim().split(/\s+/);
+  spanArray.forEach((x, ind) => {
+    if (x.innerHTML === TypedWords[ind]) {
+      x.style.color = "green";
+    } else if (TypedWords[ind]) {
+      x.style.color = "red";
     } else {
-      span.style.color = "white";
+      x.style.color = "white";
     }
   });
-  let currentWord = wordSpans[typedwords.length - 1];
-  if (currentWord) {
+  let currentWord = spanArray[TypedWords.length-1]
+  if(currentWord){
     currentWord.scrollIntoView({
-      behavior: "smooth",
-      block: "center", // keeps next line visible
-    });
+        behavior: 'smooth',
+    })
   }
 }
-btn.addEventListener("click", clicked);
-// calculating time
-function Time() {
-  let T = 60;
-  let TotatlTimer = setInterval(() => {
-    time.innerHTML = `Time <br> ${T}s`;
-    T--;
-    if (T === 0) {
-      clearInterval(TotatlTimer);
-      time.innerHTML = `Time Up <br> ${T}s`;
+
+startBtn.addEventListener("click", () => {
+  startBtn.innerHTML = "STARTED";
+  timeinterval();
+  typingArea.addEventListener("keydown", typing);
+});
+
+// let interval = setInterval(startBtn,1000);
+T = 0;
+function timeinterval() {
+  interval = setInterval(() => {
+    T++;
+    time.innerHTML = `Time <br> ${T}`;
+    if (T >= 60) {
+      time.innerHTML = `Time's up <br> ${T}s`;
       time.style.color = "red";
-      speed.style.color = "green";
-      input.removeEventListener("keydown", startTypin);
-      btn.removeEventListener("click", clicked);
-      btn.innerHTML = "START";
-      input.disabled = true;
-      chacuracy();
-      acuracy.innerHTML = `Accuracy<br>${Math.floor(
-        (CorrectWord / TypedWords) * 100
-      )}%`;
+      typingArea.disabled = true;
+      clearInterval(interval);
+      SpeedAcuracyCalculate();
     }
   }, 1000);
+}
+
+// Given function calculate speed and Acuracy
+let correctTyped = 0;
+let wrongTyped = 0;
+function SpeedAcuracyCalculate() {
+  spanArray.forEach((x) => {
+    if (x.style.color === "green") {
+      correctTyped++;
+    }
+    if (x.style.color === "red") {
+      wrongTyped++;
+    }
+  });
+  speed.innerHTML = `Speed <br>${correctTyped + wrongTyped}wpm`;
+  Total = correctTyped + wrongTyped;
+  acurate = Math.floor((correctTyped / Total) * 100);
+  acuracy.innerHTML = `Accuracy <br> ${acurate}`;
 }
